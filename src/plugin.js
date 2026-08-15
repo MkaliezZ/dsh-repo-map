b9e8fc35f310f3517d405e8f1837f7c8eb44d085
Mkaliez <nathanzhong93@gmail.com>
2026-08-15 04:44:09 -0400

    chore: normalize plugin to TypeScript (#2)

--- src/plugin.js
+++
@@ -1,1 +0,0 @@
-import {buildRepoMap,renderRepoMap} from './core.js'; export function registerRepoMap(ctx,{root=process.cwd()}={}){ctx.command?.('repo-map',async()=>renderRepoMap(await buildRepoMap(root)));}
